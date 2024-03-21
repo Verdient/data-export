c63219f507f10151132a2b7165e8f44898c20a05
Verdient <fagui.zhu@foxmail.com>
2024-03-21 11:28:36 +0800

    并行获取器 在重复ID时的情况

--- src/ParallelFetcher.php
+++ src/ParallelFetcher.php
@@ -19,6 +19,12 @@ class ParallelFetcher
     protected array $ids = [];
 
     /**
+     * @var int[] 索引集合
+     * @author Verdient。
+     */
+    protected array $indexes = [];
+
+    /**
      * @var Fetcher[] 获取器集合
      * @author Verdient。
      */
@@ -27,13 +33,27 @@ class ParallelFetcher
     /**
      * 添加获取器
      * @param array $ids 编号集合
-     * @param Fetcher $fetcher 获取器
+     * @param Fetcher|Fetcher[] $fetcher 获取器
      * @author Verdient。
      */
-    public function add(array $ids, Fetcher $fetcher): static
+    public function add(array $ids, Fetcher|array $fetcher): static
     {
+        $idsIndex = count($this->ids);
+        $fetcherIndex = count($this->fetchers);
+
         $this->ids[] = $ids;
-        $this->fetchers[] = $fetcher;
+
+        if (is_array($fetcher)) {
+            foreach ($fetcher as $fetcher2) {
+                $this->fetchers[] = $fetcher2;
+                $this->indexes[$fetcherIndex] = $idsIndex;
+                $fetcherIndex++;
+            }
+        } else {
+            $this->fetchers[] = $fetcher;
+            $this->indexes[$fetcherIndex] = $idsIndex;
+        }
+
         return $this;
     }
 
@@ -45,6 +65,7 @@ class ParallelFetcher
     {
         $this->ids[] = [];
         $this->fetchers = [];
+        $this->indexes = [];
         return $this;
     }
 
@@ -59,15 +80,15 @@ class ParallelFetcher
         }
 
         if (count($this->fetchers) === 1) {
-            $ids = $this->ids[0];
             $fetcher = $this->fetchers[0];
+            $ids = $this->ids[$this->indexes[0]];
             return [$fetcher->get($ids)];
         }
 
         $parallel = new Parallel();
 
         foreach ($this->fetchers as $index => $fetcher) {
-            $ids = $this->ids[$index];
+            $ids = $this->ids[$this->indexes[$index]];
             $parallel->add(function () use ($fetcher, $ids) {
                 return $fetcher->get($ids);
             });
